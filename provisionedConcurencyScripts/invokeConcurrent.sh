48212a487bf99bcd67d9440608a63615243814d2
PhilLarkin <philip.larkin@naimuri.com>
2020-07-23 16:42:40 +0100

    tweak to invokeConcurrent script to bunch lambda invocations together by programming language

--- provisionedConcurencyScripts/invokeConcurrent.sh
+++ provisionedConcurencyScripts/invokeConcurrent.sh
@@ -4,9 +4,9 @@ api_id=$1
 json_response="$(aws apigateway get-resources --rest-api-id $api_id )"
 resource_ids="$(python2 ../getTestIDsFromApiJsonRS.py 'Test' 'path' <<< "${json_response}")"
 
-for invokeCount in 1 2 3 4
+for resource_id in ${resource_ids}
 do
-    for resource_id in ${resource_ids}
+    for invokeCount in 1 2 3 4
     do
         python2 -c "import sys, json; print json.load(sys.stdin)['body'] , 'Invocation number $invokeCount'  " \
               <<< "$(aws apigateway test-invoke-method --rest-api-id $api_id --resource-id $resource_id --http-method 'GET' )" &
