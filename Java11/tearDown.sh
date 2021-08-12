3f961728e293bd9afebd6e6998e5f98bbedc4dc8
PhilLarkin <philip.larkin@naimuri.com>
2021-08-12 17:23:38 +0100

    some more script cleanup

--- Java11/tearDown.sh
+++ Java11/tearDown.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-if [ -z "$USER_PROFILE" ]
-then
-    aws lambda delete-function --function-name java11Test
-else
-    aws lambda delete-function --function-name java11Test --profile $USER_PROFILE
-fi
+aws lambda delete-function --function-name java11Test
